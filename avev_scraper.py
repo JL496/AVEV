64d240bef5541bd1ffcd4be04401b1d063b762e7
Julia Lundstrum <julialundstrum@gmail.com>
2026-05-18 16:43:34 -0500

    Update avev_scraper.py

--- avev_scraper.py
+++ avev_scraper.py
@@ -149,11 +149,14 @@ def main():
 
         # Locate and Select File
         file_xpath = f"//span[@title='{target_file_name}'] | //span[text()='{target_file_name}']"
+        print(file_xpath)
         #file_xpath = f"//button[contains(., '{target_file_name}')] | //span[contains(., '{target_file_name}')]"
         file_element = wait.until(EC.presence_of_element_located((By.XPATH, file_xpath)))
+        print("3")
         #file_element = wait.until(EC.element_to_be_clickable((By.XPATH, file_xpath)))
         driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", file_element)
         time.sleep(2)
+        print("4")
         #file_element.click()
         driver.execute_script("arguments[0].click();", file_element)
 
